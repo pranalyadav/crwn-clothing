bba02a1d83c5b68284e8f3eee9b9e0b300ee235c
pranalyadav <yadav.pranal94@gmail.com>
2024-11-14 12:10:00 +0530

    cart total functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from "./routes/navigation/navigation.component";
 // import SignIn from "./routes/sign-in/sign-in.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 
 // const Shop = () => {
@@ -19,6 +20,7 @@ const App = () => {
           <Route index element={<Home/>}/>
           <Route path="shop" element={<Shop/>}/>
           <Route path="auth" element={<Authentication/>}/>
+          <Route path="checkout" element={<Checkout/>}/>
         </Route>
       </Routes>
     )
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,7 +5,7 @@ const CartItem = ({cartItem}) => {
     const {name, imageUrl,price, quantity} = cartItem;
     return(
         <div className="cart-item-container">
-            <img src={imageUrl} ale={`${name}`}/>
+            <img src={imageUrl} alt={`${name}`}/>
             <div className="item-details">
                 <span className="name">{name}</span>
                 <span className="price">{quantity} x ${price}</span>
